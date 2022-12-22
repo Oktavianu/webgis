c3b241360dba185d668a631da1d40810c2fca36f
Oktavianu <oktavianusyw@gmail.com>
2022-12-22 22:41:01 +0700

    coba

--- resources/views/peta.blade.php
+++ resources/views/peta.blade.php
@@ -162,7 +162,7 @@
 			});
 			map.addControl(controlSearch);
 
-			////////////populate map with markers from sample datall
+			////////////populate map with markers from sample datall juga akaks
 			for(i in data) {
 				var title = data[i].title,	//value searched
 					loc = data[i].loc,		//position found
